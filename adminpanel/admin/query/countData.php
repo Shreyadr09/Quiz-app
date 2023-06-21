346f8fafa938820c07d9bd8473779d1f1ce76e3c
Shreya DR <shreyadr09@gmail.com>
2023-06-21 18:51:49 +0530

    database updated

--- adminpanel/admin/query/countData.php
+++ adminpanel/admin/query/countData.php
@@ -8,6 +8,25 @@ $selCourse = $conn->query("SELECT COUNT(cou_id) as totCourse FROM course_tbl ")-
 $selExam = $conn->query("SELECT COUNT(ex_id) as totExam FROM exam_tbl ")->fetch(PDO::FETCH_ASSOC);
 
 
+// // Database configuration
+// $host = 'ec2-18-206-252-110.compute-1.amazonaws.com';
+// $dbName = 'postgres';
+// $username = 'ubuntu';
+// $password = 'ubuntu';
 
+// try {
+//     // Create a new PDO instance
+//     $conn = new PDO("mysql:host=$host;dbname=$dbName", $username, $password);
+//     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// } catch (PDOException $e) {
+//     // Handle the exception and display an error message
+//     die("Database connection failed: " . $e->getMessage());
+// }
+
+// // Count All Course
+// $selCourse = $conn->query("SELECT COUNT(cou_id) as totCourse FROM course_tbl")->fetch(PDO::FETCH_ASSOC);
+
+// // Count All Exam
+// $selExam = $conn->query("SELECT COUNT(ex_id) as totExam FROM exam_tbl")->fetch(PDO::FETCH_ASSOC);
 
  ?>
--- conn.php
+++ conn.php
@@ -1,9 +1,21 @@
 <?php 
 
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "cee_db";
+// $host = "localhost";
+// $user = "root";
+// $pass = "";
+// $db   = "cee_db";
+// $conn = null;
+
+// try {
+//   $conn = new PDO("mysql:host={$host};dbname={$db};",$user,$pass);
+// } catch (Exception $e) {
+  
+// }
+
+$host = "mysql-venkatesh.alwaysdata.net";
+$user = "venkatesh";
+$pass = "Venki2002@";
+$db   = "venkatesh_examindia";
 $conn = null;
 
 try {
@@ -11,6 +23,5 @@ try {
 } catch (Exception $e) {
   
 }
-
 
  ?>
